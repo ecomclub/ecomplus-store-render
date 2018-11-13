7002e6a1e24a64120d3dd9560337ac5e83fa832c
Leonardo Matos <leomp120894@gmail.com>
2018-11-13 13:26:46 -0200

    add utility functions, getCookie

--- src/index.js
+++ src/index.js
@@ -56,6 +56,36 @@ var __ecom = {};
     }
   }
 
+  // add utility functions
+  var utils = {}
+  __ecom._ = utils
+
+  // get cookie value by cookie name
+  utils.getCookie = function (cname) {
+    // check for document cookies
+    var cookies = root['document'].cookie
+
+    if (typeof cookies === 'string') {
+      // Ref.: https://www.w3schools.com/js/js_cookies.asp
+      var name = cname + '='
+      var decodedCookie = decodeURIComponent(cookies)
+      var ca = decodedCookie.split(';')
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1)
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length)
+        }
+      }
+    }
+
+    // cookie not found
+    // returns empty string by default
+    return ''
+  }
+
   // Export the Ecom object for **Node.js**, with
   // backwards-compatibility for their old module API. If we're in
   // the browser, add `Ecom` as a global object.
