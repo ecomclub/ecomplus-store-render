e1c9b2b107f8730f25e1a85edf6c8f384dab3aa0
Leonardo Matos <leomp120894@gmail.com>
2018-11-19 15:48:08 -0200

    export Vue instance available methods

--- src/partials/main.js
+++ src/partials/main.js
@@ -531,6 +531,8 @@
 
   // option to render specific elements manually
   Ecom.render = renderElement
+  // export Vue instance available methods
+  Ecom.methods = vueEcom.methods
 
   Ecom.init = function (callback, storeId, storeObjectId, lang, doc) {
     var i, store
