1f60623fdca3a95a4c4059e4063eb4033a3f3d73
Leonardo Matos <leomp120894@gmail.com>
2019-02-14 20:51:50 -0200

    build: replacing gulp with webpack + babel

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'storefront.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'tests'),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              { useBuiltIns: 'usage' }
+            ]
+          ]
+        }
+      }
+    }]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+}
