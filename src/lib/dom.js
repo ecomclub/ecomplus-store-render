2503c51a97dccc718852fb6594f89857a7cf75f5
Leonardo Matos <leomp120894@gmail.com>
2019-02-16 23:27:59 -0300

    fix: set dom properties after resolved jsdom promise, add location

--- src/lib/dom.js
+++ src/lib/dom.js
@@ -12,36 +12,51 @@
  */
 
 const DOM = (html, url, file, dom) => {
-  if (dom) {
-    // hard set
-    DOM.document = dom
-  } else if (typeof window === 'object' && window.document) {
-    // browser
-    DOM.document = window.document
-  } else if (typeof require === 'function' && (html || url || file)) {
-    // NodeJS environment
-    // use JSDOM constructor
-    // https://github.com/jsdom/jsdom
-    const jsdom = require('jsdom')
-    const { JSDOM } = jsdom
+  return new Promise((resolve, reject) => {
+    if (dom) {
+      // hard set
+      resolve(dom)
+    } else if (typeof require === 'function' && (html || url || file)) {
+      // NodeJS environment
+      // use JSDOM constructor
+      // https://github.com/jsdom/jsdom
+      const jsdom = require('jsdom')
+      const { JSDOM } = jsdom
 
-    let dom
-    if (html) {
-      dom = new JSDOM(html)
+      if (html) {
+        resolve(new JSDOM(html))
+      } else {
+        // get DOM from web link or local HTML file
+        (url ? JSDOM.fromURL(url) : JSDOM.fromFile(file)).then(resolve)
+      }
     } else {
-      // get DOM from web link or local HTML file
-      (url ? JSDOM.fromURL(url) : JSDOM.fromFile(file))
-        .then(newDom => { dom = newDom })
+      reject(new Error('Cannot setup DOM'))
     }
+  })
 
-    /**
-     * Document object.
-     * @type {object}
-     * @memberOf DOM
-     */
+    .then(dom => {
+      /**
+       * Document object.
+       * @type {object}
+       * @memberOf DOM
+       */
 
-    DOM.document = dom.window.document
-  }
+      DOM.document = dom.window.document
+
+      /**
+       * Location object.
+       * @type {object}
+       * @memberOf DOM
+       */
+
+      DOM.location = dom.window.location
+    })
+}
+
+if (typeof window === 'object' && window.document) {
+  // browser
+  DOM.document = window.document
+  DOM.location = window.location
 }
 
 module.exports = DOM
