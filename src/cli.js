b3432e2dedcd23ff10c7187212a6311ccb78ac76
Leonardo Matos <leomp120894@gmail.com>
2019-03-01 17:15:16 -0300

    fix: using Node.js path module to improve CLI compatibility

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,20 @@
+#!/usr/bin/env node
+
 'use strict'
 
 if (typeof process === 'object' && Array.isArray(process.argv)) {
   // NodeJS CLI
   // node ./index.js https://www.mystore.com.br/product
   if (process.argv.length >= 3) {
+    // core Node.js path module
+    const path = require('path')
     // setup DOM first
     let url = process.argv[2]
 
-    require('./lib/dom')(null, url)
+    require(path.join(__dirname, 'lib', 'dom'))(null, url)
       .then(dom => {
         // render entire document
-        require('./ecom').init().then(() => {
+        require(path.join(__dirname, 'ecom')).init().then(() => {
           console.log(dom.serialize())
           process.exit(0)
         })
